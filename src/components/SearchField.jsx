be35c8baa5fb664b84314562931e79dbcec6a2cf
Albin Antony <albinantonydominic@gmail.com>
2023-07-26 20:13:42 +0530

    final changes

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,35 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./SearchField.css";
+import searchIcon from "../assets/icons8-search.svg";
+export default function SearchField({ setImg }) {
+  const [value, setValue] = useState("horse");
+  useEffect(() => {
+    setImg(value);
+  }, []);
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    setImg(value);
+  }
 
-import React, { useEffect, useState } from 'react'
-import './SearchField.css'
-export default function SearchField({setImg}) {
-    const [value, setValue] = useState("horse");
-    useEffect(() => {
-        setImg(value);
-      }, []);
-    
-    function handleSubmit(e) {
-      e.preventDefault();
-      setImg(value);
-    }
-      
-    return (
-        <div className="search-container">
-          <form onSubmit={handleSubmit}>
-            <div className="search">
-              <div>
-                <input
-                  id="search"
-                  placeholder="Search..."
-                  type="text"
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-                />
-              </div>
-              <button type="submit">
-                <img className="search-icon" src={searchIcon} />
-              </button>
-            </div>
-          </form>
+  return (
+    <div className="search-container">
+      <form onSubmit={handleSubmit}>
+        <div className="search">
+          <div>
+            <input
+              id="search"
+              placeholder="Search..."
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <button type="submit">
+            <img className="search-icon" src={searchIcon} />
+          </button>
         </div>
-      );
+      </form>
+    </div>
+  );
 }
