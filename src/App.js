a5bf5e7916ba24b55d95245d8ab6a5dc9d906fa0
Albin Antony <albinantonydominic@gmail.com>
2023-07-26 19:25:10 +0530

    api integration with search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchField from "./components/SearchField";
+import ReactPaginate from "react-paginate";
+import spinner from "./assets/Eclipse-1s-200px (2).svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [res, setRes] = useState([]);
+  const [img, setImg] = useState("");
+  
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchRequest();
+  }, [img]);
+
+
+  const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
+
+
+  const fetchRequest = async (currentPage = 1) => {
+    try {
+      setLoading(true);
+      const data = await fetch(
+        `https://api.unsplash.com/search/photos?page=${currentPage}&query=${img}&client_id=${ACCESS_KEY}&per_page=10`
+      );
+      const dataJ = await data.json();
+      const result = dataJ.results;
+      setRes(result);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  
+
+  return (<>
+  </>);
 }
 
 export default App;
--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,21 +1,35 @@
 
 import React, { useEffect, useState } from 'react'
 import './SearchField.css'
-export default function SearchField() {
- 
+export default function SearchField({setImg}) {
+    const [value, setValue] = useState("horse");
+    useEffect(() => {
+        setImg(value);
+      }, []);
+    
+    function handleSubmit(e) {
+      e.preventDefault();
+      setImg(value);
+    }
       
-  return (
-    <div className='search-container'>
-       <form onSubmit={}>
-        <div  className='search'>
-      <div>
-        <input id="search" placeholder='Search...' type="text" value={} onChange={}/>
-      </div>
-      <button type="submit">
-        <img className="search-icon"src={}/>
-      </button>
-      </div>
-    </form>
-    </div>
-  )
+    return (
+        <div className="search-container">
+          <form onSubmit={handleSubmit}>
+            <div className="search">
+              <div>
+                <input
+                  id="search"
+                  placeholder="Search..."
+                  type="text"
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </div>
+              <button type="submit">
+                <img className="search-icon" src={searchIcon} />
+              </button>
+            </div>
+          </form>
+        </div>
+      );
 }
