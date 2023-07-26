14ff23ea91db9d88b0bfdf670141eb6498d4ad24
Albin Antony <albinantonydominic@gmail.com>
2023-07-26 19:14:44 +0530

    style searchfield

---
+++ src/components/SearchField.jsx
@@ -0,0 +1,21 @@
+
+import React, { useEffect, useState } from 'react'
+import './SearchField.css'
+export default function SearchField() {
+ 
+      
+  return (
+    <div className='search-container'>
+       <form onSubmit={}>
+        <div  className='search'>
+      <div>
+        <input id="search" placeholder='Search...' type="text" value={} onChange={}/>
+      </div>
+      <button type="submit">
+        <img className="search-icon"src={}/>
+      </button>
+      </div>
+    </form>
+    </div>
+  )
+}
